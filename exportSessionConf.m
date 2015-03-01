4aca7fe74ca8f1d151a3ea7e3c323b1d57a58edb
Matt Gaidica <matt@gaidi.ca>
2015-03-01 18:52:00 -0500

    Script writes con file for session

--- exportSessionConf.m
+++ exportSessionConf.m
@@ -1,20 +1,16 @@
-function exportSessionConf(sessionName)
-%
+function exportSessionConf(sessionName,saveDir)
 % This function exports a configuration file for a session so
-% that processing can be offloaded to a non-networked machine
+% that processing can be offloaded to a non-networked machine.
+% INPUTS:
+%   sessionName : ex. R0036_20150225a
+%   saveDir: where you want this config file save, right now the
+%   extractSpikesTDT script prompts for the location of this file
 
-% store these somewhere else though!
-hostIP = '172.20.138.142';
-user = 'dleventh';
-password = 'amygdala_probe';
-dbName = 'spikedb';
+[~,ratID] = sql_getSubjectFromSession(sessionName);
+chMap = sql_getChannelMap(ratID);
+% you can pass tetrodes into extractSpikesTDT, assume you can't
+% do it here (the DB needs to be populated)
+tetrodeList = chMap.tetNames;
 
-sqlArgs = {'hostip', hostIP, ...
-            'user', user, ...
-            'password', password, ...
-            'dbname', dbName,
-            'sqljava_version', ''
-            };
-
-[~, ratID] = sql_getSubjectFromSession(sessionName,sqlArgs{:});
-disp(ratID);
+filename = ['session_conf_',sessionName,'.mat'];
+save(fullfile(saveDir,filename),'chMap','ratID','tetrodeList');
